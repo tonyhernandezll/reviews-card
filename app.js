26cc48b89a8376c8651fc45cd4315e7b346f6278
Tony <tonyhll1972@gmail.com>
2021-05-15 22:55:49 -0500

    added functionality to the prev button, and the surprise me button

--- app.js
+++ app.js
@@ -81,4 +81,17 @@ nextBtn.addEventListener("click", function () {
 });
 
 // show prev person
-prevBtn.addEventListener
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+
+// show random person
+randomBtn.addEventListener("click", function () {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem);
+
+});
